a4ef6f082cdbc76e4af62e08ffed5ba7a36d57e5
Your Name <you@example.com>
2020-05-24 16:01:12 -0700

    Outputs difference in times

--- test_times.py
+++ test_times.py
@@ -8,15 +8,25 @@ import datetime
 dbname='/home/pi/project/sensordata.db'
 now = datetime.datetime.now()
 
+time_sql = "SELECT datetime('now') FROM dhtreadings"
+
 def get_data():
     conn=sqlite3.connect(dbname)
     curs = conn.cursor()
 
-    curs.execute("SELECT datetime('now') FROM dhtreadings")
+    curs.execute(time_sql)
     rows = curs.fetchone()
     conn.close()
 
-    print("Time in the database is: " + str(rows))
-    print("Time in OS is: " + str(now))
+    return rows[0]
 
-get_data()
+def main():
+    db_time = get_data()
+    print("Time in the database is: " + str(db_time))
+    print("Time in OS is: " + str(now))
+    datetime_db_time = datetime.datetime.strptime(db_time, '%Y-%m-%d %H:%M:%S')
+    difference = datetime_db_time - now
+    print("Difference in time is: " + str(difference))
+
+if __name__=="__main__":
+    main()
